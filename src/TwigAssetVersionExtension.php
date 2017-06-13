023705e384eb16117a0a87f5b1be8986e503d67e
Mark Railton <mark@markrailton.com>
2017-06-13 22:12:46 +0100

    Initial Commit

---
+++ src/TwigAssetVersionExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+class TwigAssetVersionExtension extends \Twig_Extension
+{
+    private $manifest;
+
+    public function __construct($manifest)
+    {
+        $this->manifest = $manifest;
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('asset_version', array($this, 'getAssetVersion')),
+        );
+    }
+
+    public function getAssetVersion($filename)
+    {
+        $manifestPath = $this->manifest;
+        if (!file_exists($manifestPath)) {
+            throw new \Exception(sprintf('Cannot find manifest file: "%s"', $manifestPath));
+        }
+        $paths = json_decode(file_get_contents($manifestPath), true);
+        if (!isset($paths[$filename])) {
+            throw new \Exception(sprintf('There is no file "%s" in the version manifest!', $filename));
+        }
+        return $paths[$filename];
+    }
+    
+    public function getName()
+    {
+        return 'asset_version';
+    }
+}
